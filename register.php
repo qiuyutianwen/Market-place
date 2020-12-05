bef9372d2bf5753f8cb73478563e97d23ebaf5f7
仇禹 <qiuyutianwen@chouyudeMacBook-Pro.local>
2020-12-05 09:32:24 -0800

    s

--- register.php
+++ register.php
@@ -3,12 +3,13 @@
 	require_once "config.php";
 	if(isset($_POST['submit']))
 	{
-		extract($_POST);
-		if(empty(trim($username)))
-		{
-			echo "Please enter a username!";
-		} else {
-			echo $username . " " . $password . " " $confirmpassword;
-		}
+		include "home.html";
+		// extract($_POST);
+		// if(empty(trim($username)))
+		// {
+		// 	echo "Please enter a username!";
+		// } else {
+		// 	echo $username . " " . $password . " " $confirmpassword;
+		// }
 	}
 ?>
