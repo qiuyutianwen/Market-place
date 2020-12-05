30067ad09a0f203dd6ae44552e0cad7755f8ecbf
仇禹 <qiuyutianwen@chouyudeMacBook-Pro.local>
2020-12-04 18:06:39 -0800

    s

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>CMPE272</title>
+  </head>
+  <body>
+<?php
+//include 'insertData.php';
+//include 'fetchdata.php';
+//include 'curlFetchData.php';
+include 'home.html';
+?>
+</body>
+</html>
