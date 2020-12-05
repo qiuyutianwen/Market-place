4e0558371d4eb46e3caefd6ba6577930dc5e2830
仇禹 <qiuyutianwen@chouyudeMacBook-Pro.local>
2020-12-05 09:48:54 -0800

    s

--- register.php
+++ register.php
@@ -3,14 +3,15 @@
 	require_once "config.php";
 	if(isset($_POST['submit']))
 	{
-		extract($_POST);
-		if(empty(trim($username)))
-		{
-			echo "Please enter a username!";
-		} else {
-			echo "<tr><td style=\"background-color:#F0E68C\">" . trim($username) .
-    "</td><td style=\"background-color:#FFA500\">" . trim($password) .
-    "</td><td style=\"background-color:#FFA500\">" . trim($confirmpassword) ."</td></tr>";
-		}
+		echo '<script>alert("Account created successfully!")</script>'; 
+		// extract($_POST);
+		// if(empty(trim($username)))
+		// {
+		// 	echo "Please enter a username!";
+		// } else {
+		// 	echo "<tr><td style=\"background-color:#F0E68C\">" . trim($username) .
+  //   "</td><td style=\"background-color:#FFA500\">" . trim($password) .
+  //   "</td><td style=\"background-color:#FFA500\">" . trim($confirmpassword) ."</td></tr>";
+		// }
 	}
 ?>
