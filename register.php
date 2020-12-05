0305c2690863992a55d6b305613597e49eb1a3a9
仇禹 <qiuyutianwen@chouyudeMacBook-Pro.local>
2020-12-05 10:59:05 -0800

    S

--- register.php
+++ register.php
@@ -5,101 +5,100 @@
 	$username_err = $password_err = $confirm_password_err = "";
 	
 	if(isset($_POST['submit']))
-	{
-		$username_err = "Please enter a username.";
-		echo "<script>alert('$username_err');</script>"; 
-		// Validate username
-	    // if(empty(trim($_POST["username"]))){
-	    //     $username_err = "Please enter a username.";
-	    // } else{
-	    //     // Prepare a select statement
-	    //     $sql = "SELECT id FROM users WHERE username = ?";
+	{ 
+		//Validate username
+	    if(empty(trim($_POST["username"]))){
+	        $username_err = "Please enter a username.";
+	    } else{
+	        // Prepare a select statement
+	        $sql = "SELECT id FROM users WHERE username = ?";
 	        
-	    //     if($stmt = mysqli_prepare($link, $sql)){
-	    //         // Bind variables to the prepared statement as parameters
-	    //         mysqli_stmt_bind_param($stmt, "s", $param_username);
+	        if($stmt = mysqli_prepare($link, $sql)){
+	            // Bind variables to the prepared statement as parameters
+	            mysqli_stmt_bind_param($stmt, "s", $param_username);
 	            
-	    //         // Set parameters
-	    //         $param_username = trim($_POST["username"]);
+	            // Set parameters
+	            $param_username = trim($_POST["username"]);
 	            
-	    //         // Attempt to execute the prepared statement
-	    //         if(mysqli_stmt_execute($stmt)){
-	    //             /* store result */
-	    //             mysqli_stmt_store_result($stmt);
+	            // Attempt to execute the prepared statement
+	            if(mysqli_stmt_execute($stmt)){
+	                /* store result */
+	                mysqli_stmt_store_result($stmt);
 	                
-	    //             if(mysqli_stmt_num_rows($stmt) == 1){
-	    //                 $username_err = "This username is already taken.";
-	    //             } else{
-	    //                 $username = trim($_POST["username"]);
-	    //             }
-	    //         } else{
-	            	
-	    //             echo '<script>alert(' . mysqli_stmt_error($stmt) . ')</script>';
-	    //         }
+	                if(mysqli_stmt_num_rows($stmt) == 1){
+	                    $username_err = "This username is already taken.";
+	                } else{
+	                    $username = trim($_POST["username"]);
+	                }
+	            } else{
+	            	$error1 =  mysqli_stmt_error($stmt);
+	            	echo "<script>alert('$error1');</script>";
+	            }
 
-	    //         // Close statement
-	    //         mysqli_stmt_close($stmt);
-	    //     }
-	    // }
+	            // Close statement
+	            mysqli_stmt_close($stmt);
+	        }
+	    }
 	    
-	    // // Validate password
-	    // if(empty(trim($_POST["password"]))){
-	    //     $password_err = "Please enter a password.";     
-	    // } elseif(strlen(trim($_POST["password"])) < 6){
-	    //     $password_err = "Password must have atleast 6 characters.";
-	    // } else{
-	    //     $password = trim($_POST["password"]);
-	    // }
+	    // Validate password
+	    if(empty(trim($_POST["password"]))){
+	        $password_err = "Please enter a password.";     
+	    } elseif(strlen(trim($_POST["password"])) < 6){
+	        $password_err = "Password must have atleast 6 characters.";
+	    } else{
+	        $password = trim($_POST["password"]);
+	    }
 	    
-	    // // Validate confirm password
-	    // if(empty(trim($_POST["confirm_password"]))){
-	    //     $confirm_password_err = "Please confirm password.";     
-	    // } else{
-	    //     $confirm_password = trim($_POST["confirm_password"]);
-	    //     if(empty($password_err) && ($password != $confirm_password)){
-	    //         $confirm_password_err = "Password did not match.";
-	    //     }
-	    // }
+	    // Validate confirm password
+	    if(empty(trim($_POST["confirm_password"]))){
+	        $confirm_password_err = "Please confirm password.";     
+	    } else{
+	        $confirm_password = trim($_POST["confirm_password"]);
+	        if(empty($password_err) && ($password != $confirm_password)){
+	            $confirm_password_err = "Password did not match.";
+	        }
+	    }
 	    
-	    // // Check input errors before inserting in database
-	    // if(!empty($username_err))
-	    // {
-	    // 	echo '<script>alert(' . $username_err . ')</script>';
-	    // } elseif (!empty($password_err)) 
-	    // {
-	    // 	echo '<script>alert(' . $password_err . ')</script>';
-	    // } elseif (!empty($confirm_password_err)) 
-	    // {
-	    // 	echo '<script>alert(' . $confirm_password_err . ')</script>';
-	    // }
-	    // if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
+	    // Check input errors before inserting in database
+	    if(!empty($username_err))
+	    {
+			echo "<script>alert('$username_err');</script>";
+	    } elseif (!empty($password_err)) 
+	    {
+	    	echo "<script>alert('$password_err');</script>";
+	    } elseif (!empty($confirm_password_err)) 
+	    {
+	    	echo "<script>alert('$confirm_password_err');</script>";
+	    }
+	    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
 	        
-	    //     // Prepare an insert statement
-	    //     $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+	        // Prepare an insert statement
+	        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
 	         
-	    //     if($stmt = mysqli_prepare($link, $sql)){
-	    //         // Bind variables to the prepared statement as parameters
-	    //         mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+	        if($stmt = mysqli_prepare($link, $sql)){
+	            // Bind variables to the prepared statement as parameters
+	            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
 	            
-	    //         // Set parameters
-	    //         $param_username = $username;
-	    //         $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+	            // Set parameters
+	            $param_username = $username;
+	            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
 	            
-	    //         // Attempt to execute the prepared statement
-	    //         if(mysqli_stmt_execute($stmt)){
-	    //             // Redirect to login page
-	    //             include "home.html";
-	    //         } else{
-	    //             echo '<script>alert(' . mysqli_stmt_error($stmt) . ')</script>';
-	    //         }
+	            // Attempt to execute the prepared statement
+	            if(mysqli_stmt_execute($stmt)){
+	                // Redirect to login page
+	                include "home.html";
+	            } else{
+	            	$error2 = mysqli_stmt_error($stmt);
+	                echo "<script>alert('$error2');</script>";
+	            }
 
-	    //         // Close statement
-	    //         mysqli_stmt_close($stmt);
-	    //     }
-	    // }
+	            // Close statement
+	            mysqli_stmt_close($stmt);
+	        }
+	    }
 	    
-	    // // Close connection
-	    // mysqli_close($link);
+	    // Close connection
+	    mysqli_close($link);
 	}
 ?>
 
