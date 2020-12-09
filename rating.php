4453a3ad1e7ba4d700458c812b470ddfd87fc893
仇禹 <qiuyutianwen@chouyudeMacBook-Pro.local>
2020-12-09 14:25:02 -0800

    s

--- rating.php
+++ rating.php
@@ -172,7 +172,7 @@ mysqli_close($link);
 	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 	<script>
 		var ratedIndex = -1;
-		var review = $("#review").val();
+		var review = $('textarea#review').val();
 		$(document).ready(function() {
 			resetStarColors();
 
