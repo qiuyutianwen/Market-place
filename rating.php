1e73445cd4ff49f1dcf63183d03fe18327107443
仇禹 <qiuyutianwen@chouyudeMacBook-Pro.local>
2020-12-09 14:59:01 -0800

    s

--- rating.php
+++ rating.php
@@ -172,7 +172,7 @@ mysqli_close($link);
 	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 	<script>
 		var ratedIndex = -1;
-		var review = $.trim($("#comment").val());
+		
 
 		$(document).ready(function() {
 			resetStarColors();
@@ -187,8 +187,9 @@ mysqli_close($link);
 			$('.fa-star').on('click', function() {
 				ratedIndex = parseInt($(this).data('index'));
 				localStorage.setItem('ratedIndex', ratedIndex);
+				var review = $.trim($("#comment").val());
 				alert(review);
-				saveToTheDB();
+				saveToTheDB(review);
 			});
 
 			$('.fa-star').mouseover(function() {
@@ -214,7 +215,7 @@ mysqli_close($link);
 		});
 
 		
-		function saveToTheDB() {
+		function saveToTheDB(review) {
 			$.ajax({
 				url: "rating.php",
 				method: "POST",
