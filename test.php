8fc8ea9ccfcb9a8585a983cad6fcec3d60338478
仇禹 <qiuyutianwen@chouyudeMacBook-Pro.local>
2020-12-13 04:20:09 -0800

    S

--- test.php
+++ test.php
@@ -50,32 +50,33 @@ if (mysqli_num_rows($result) > 0) {
   echo "<script>alert('No data!');</script>";
 }
 
-//get top 5 most visited data
-// $sql = "SELECT company, product, visitTimes FROM visitTimes ORDER BY visitTimes DESC LIMIT 5;";
-// $result = mysqli_query($link, $sql);
-// $Top5_Most_Visit_array = array();
-// if (mysqli_num_rows($result) > 0) {
-//   // output data of each row
-//   while($row = mysqli_fetch_assoc($result)) {
-//     $array_item = array();
-//   	$Top5_Most_Visit_name = $row["company"]."-".$row["product"];
-//   	$Top5_Most_VisitTimes = $row["visitTimes"];
+get top 5 most visited data
+$sql = "SELECT company, product, visitTimes FROM visitTimes ORDER BY visitTimes DESC LIMIT 5;";
+$result = mysqli_query($link, $sql);
+$Top5_Most_Visit_array = array();
+if (mysqli_num_rows($result) > 0) {
+  // output data of each row
+  while($row = mysqli_fetch_assoc($result)) {
+    $array_item = array();
+  	$Top5_Most_Visit_name = $row["company"]."-".$row["product"];
+  	$Top5_Most_VisitTimes = $row["visitTimes"];
     
-//     array_push($array_item, $Top5_Most_Visit_name);
-//     array_push($array_item, $Top5_Most_VisitTimes);
-//     array_push($Top5_Most_Visit_array, $array_item);
-//   }
-// } else {
-//   echo "<script>alert('This page hasn't been visited now!');</script>";
-// }
+    array_push($array_item, $Top5_Most_Visit_name);
+    array_push($array_item, $Top5_Most_VisitTimes);
+    array_push($Top5_Most_Visit_array, $array_item);
+  }
+} else {
+  echo "<script>alert('This page hasn't been visited now!');</script>";
+}
 
 for($i=0;$i<5;$i++)
 {
-	echo $i .": ".$Top5_Most_Avg_array[$i][0] . " " $Top5_Most_Avg_array[$i][1]."<br>";
+	echo $i .": ".$Top5_Most_Avg_array[$i][0] . " ". $Top5_Most_Avg_array[$i][1]."<br>";
 }
-// for($i=0;$i<5;$i++)
-// {
-// 	echo $i .": ".$Top5_Most_Visit_array[$i][0] . " " $Top5_Most_Visit_array[$i][1];
-// }
-// mysqli_close($link);
+echo " <br>";
+for($i=0;$i<5;$i++)
+{
+	echo $i .": ".$Top5_Most_Visit_array[$i][0] . " " .$Top5_Most_Visit_array[$i][1]."<br>";
+}
+mysqli_close($link);
 ?>
