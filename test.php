5a8770a7489c85321bfd6d067ed9e9264c36a84f
仇禹 <qiuyutianwen@chouyudeMacBook-Pro.local>
2020-12-13 03:32:58 -0800

    s

--- test.php
+++ test.php
@@ -1,8 +1,19 @@
 <?php
 require_once "config.php";
+$sql = "SELECT * FROM rating;";
+$result = mysqli_query($link, $sql);
+if (mysqli_num_rows($result) > 0) {
+  // output data of each row
+  while($row = mysqli_fetch_assoc($result)) {
+    echo "company: ".$row["company"]. " product: " .$row["product"]. " username: " .$row["username"]. " review: " .$row["review"]." Rating: ".$row["Rating"]."<br>";
+  }
+} else {
+  echo "<script>alert('This page hasn't been visited now!');</script>";
+}
+
+
 $sql = "SELECT company, product, AVG(rating) AS AverageRating FROM rating GROUP BY company, product ORDER BY AverageRating DESC LIMIT 5;";
 $result = mysqli_query($link, $sql);
-$visitTimes = "";
 if (mysqli_num_rows($result) > 0) {
   // output data of each row
   while($row = mysqli_fetch_assoc($result)) {
