501a6ce20524ac889dd43b1a7dfcb2a452465361
仇禹 <qiuyutianwen@chouyudeMacBook-Pro.local>
2020-12-13 03:40:28 -0800

    S

--- index.php
+++ index.php
@@ -7,10 +7,10 @@
 <?php
 //include 'createUserTable.php';
 //include 'createVisitTable.php';
-include 'createRatingTable.php';
+//include 'createRatingTable.php';
 //include 'FetchUserData.php';
 //include 'test.php';
-//include 'login.php';
+include 'login.php';
 
 ?>
 </body>
