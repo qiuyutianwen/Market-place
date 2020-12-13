6a343e88341906050a75feffc0340cf02abe9015
仇禹 <qiuyutianwen@chouyudeMacBook-Pro.local>
2020-12-13 02:29:24 -0800

    S

--- test.php
+++ test.php
@@ -53,12 +53,12 @@ if($count > 0)
 	$average_rating = $total_rating / $count;
 	$average_rating = round($average_rating, 1);
 }
-echo "new test<br>";
-$array_len = sizeof($data_array);
-for($i=0; $i < $array_len;$i++)
-{
-	echo "company: " . $data_array[$i][0]. " product: ".$data_array[$i][1]." username: ".$data_array[$i][2]." review: ".$data_array[$i][3]." rating: ".$data_array[$i][4]." reg_date: ".$data_array[$i][5] . "<br>";
-}
+echo "new test: <br>";
+// $array_len = sizeof($data_array);
+// for($i=0; $i < $array_len;$i++)
+// {
+// 	echo "company: " . $data_array[$i][0]. " product: ".$data_array[$i][1]." username: ".$data_array[$i][2]." review: ".$data_array[$i][3]." rating: ".$data_array[$i][4]." reg_date: ".$data_array[$i][5] . "<br>";
+// }
 echo $average_rating."<br>";
 echo $count."<br>";
 echo $star_5."<br>";
@@ -66,5 +66,22 @@ echo $star_4."<br>";
 echo $star_3."<br>";
 echo $star_2."<br>";
 echo $star_1."<br>";
+$array_len = sizeof($data_array);
+					for($i=0; $i < $array_len;$i++)
+					{
+						echo "<li>
+							<div>
+								<img src="images/defaultUser.png">
+								<span class="username">".$data_array[$i][2]."</span>
+								<span class="stars">Rating: ".$data_array[$i][4]."</span>
+								<span class="reviewDate">Posted at: ".$data_array[$i][5]."</span>
+							</div>
+							<div>
+								<p>
+								".$data_array[$i][3]."
+								</p>
+							</div>
+						</li>";
+					}
 mysqli_close($link);
 ?>
