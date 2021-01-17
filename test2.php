6283685aea89030c26c61cf9b2f24e41b0e63b57
仇禹 <qiuyutianwen@chouyudeMacBook-Pro.local>
2021-01-17 10:23:03 -0800

    s

--- test2.php
+++ test2.php
@@ -17,6 +17,7 @@ $fb = new \Facebook\Facebook([
   'app_id' => '830689571103351',           //Replace {your-app-id} with your app ID
   'app_secret' => '3191d39933fbd814c239f0a6a059865a',   //Replace {your-app-secret} with your app secret
   'graph_api_version' => 'v5.0',
+  'default_access_token' => 'EAALzgfzN4ncBAGzTrmuqCI9Y2vNcxLsVmPg6sE9WSQIXB8Pqn8N7GdV8dBsvN4Y5ofrZAIgHxLm09TGDGaThKlQETJEQhY9FMWQY1DQ9hE1exl0AuSYdxElmpHDeiUU9IwQZAhFVJhKaKp7w2rPiXJBPOsPZCMeuqWU7k7ReZAY0nO2IKZBnJ51k9kKycrZBccuKtIZBckONoRmOnL28rYlI2T0j4lHcrYK6yO7C8EmQAZDZD',
 ]);
 
 
