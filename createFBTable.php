20d258d122f5fc53740b0b394b8d0b57cc5a6620
仇禹 <qiuyutianwen@chouyudeMacBook-Pro.local>
2021-01-18 12:30:00 -0800

    s

--- createFBTable.php
+++ createFBTable.php
@@ -5,17 +5,17 @@ require_once "config.php";
 $sql1 = "DROP TABLE IF EXISTS FBTable";
 if(mysqli_query($link, $sql1))
 {
-	echo "Table FBTable deleted successfully";
+	echo "Table FBTable deleted successfully<br />";
 } else {
 	echo "Error deleting table: " . mysqli_error($link);
 }
 
 //set char set
 mysqli_set_charset($link,"utf8mb4");
-
 echo "Current character set is: " . mysqli_character_set_name($link);
+
 // sql to create table
-$sql = "CREATE TABLE FBTable (
+$sql2 = "CREATE TABLE FBTable (
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     fbID VARCHAR(50) NOT NULL UNIQUE,
     username VARCHAR(255) NOT NULL,
@@ -23,11 +23,32 @@ $sql = "CREATE TABLE FBTable (
     reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )";
 
-if (mysqli_query($link, $sql)) {
-  echo "Table FBTable created successfully";
+if (mysqli_query($link, $sql2)) {
+  echo "Table FBTable created successfully<br />";
 } else {
   echo "Error creating table: " . mysqli_error($link);
 }
 
+//insert test data
+$sql3 = "INSERT INTO FBTable (fbID, username, email)
+VALUES ('4252345', '仇禹', 'qiuyutianwen@gmail.com');";
+
+if (mysqli_multi_query($link, $sql3)) {
+  echo "New records created successfully<br />";
+} else {
+  echo "Error: " . $sql3 . "<br />" . mysqli_error($link);
+}
+
+//get top 5 most average rating data
+$sql4 = "SELECT id, fbID, username, email, reg_date FROM FBTable ORDER BY id";
+$result = mysqli_query($link, $sql4);
+if (mysqli_num_rows($result) > 0) {
+  // output data of each row
+  while($row = mysqli_fetch_assoc($result)) {
+  	echo $row["id"] . " " . $row["fbID"] . " " . $row["username"] . " " . $row["email"] . " " . $row["reg_date"];
+  }
+} else {
+  echo "No data!";
+}
 mysqli_close($link);
 ?>
